0ca64a867aa066031e86845488cc96eb244b1334
lymengg <lymengouk2610@gmail.com>
2025-08-02 20:40:21 +0700

    feat: route matching for cloudflare page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
 
   css: ["~/assets/css/main.css"],
 
